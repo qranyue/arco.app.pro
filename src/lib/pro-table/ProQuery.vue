7c1da2d011ad7bdbe908e755ce56664fcd8ebd55
Qrany <qran.yue@hotmail.com>
2022-12-19 21:53:23 +0800

    表格查询统一处理
    查询组件

--- src/lib/pro-table/ProQuery.vue
+++ src/lib/pro-table/ProQuery.vue
@@ -1,7 +1,10 @@
 <script lang="ts" setup>
-import { computed, inject, reactive } from "vue";
-import { TableColumns, TableLoading, TableOnReload } from "./keys";
+import { computed, reactive, shallowRef } from "vue";
 import type { ProTableColumnData } from "./models";
+
+const collapsed = shallowRef(true);
+const triggerCollapsed = () => (collapsed.value = !collapsed.value);
+const collapsedText = computed(() => (collapsed.value ? "展开" : "收起"));
 
 interface Props {
   columns?: ProTableColumnData[];
@@ -9,17 +12,15 @@ interface Props {
 }
 const $props = defineProps<Props>();
 
-const _columns = inject(TableColumns);
-const $_columns = computed(() => {
+const $columns = computed(() => {
   const cs: ProTableColumnData[] = [];
-  (_columns?.value || $props.columns || []).forEach((x) => {
+  ($props.columns || []).forEach((x) => {
     if (x.dataIndex) cs.push({ ...x });
   });
   return cs;
 });
 
-const _loading = inject(TableLoading);
-const $_loading = computed(() => _loading?.value ?? $props.loading ?? false);
+const $loading = computed(() => $props.loading ?? false);
 
 interface Emits {
   (event: "query"): void;
@@ -29,46 +30,46 @@ const $emit = defineEmits<Emits>();
 
 const form = reactive<Record<string, any>>({});
 
-const _onReload = inject(TableOnReload);
 const onReload = () => {
   Object.keys(form).forEach((k) => (form[k] = undefined));
-  _onReload?.();
   $emit("reload");
 };
 </script>
 
 <template>
-  <AForm :model="form" :label-col-props="{ span: 8 }" :wrapper-col-props="{ span: 16 }">
-    <ARow align="center">
-      <ACol flex="auto">
-        <ARow class="pro-query-main" :gutter="[16, 0]">
-          <ACol v-for="x in $_columns" :key="x.dataIndex" :lg="8">
-            <AFormItem :label="x.title">
-              <template v-if="x.tips" #label>
-                <span>{{ x.title }}</span>
-                <APopover>
-                  <template #content>{{ x.tips }}</template>
-                  <IconQuestionCircle />
-                </APopover>
-              </template>
-              <ARangePicker v-if="x.valueType === 'dateRange'" v-model="form[x.dataIndex!]" />
-              <AInput v-else v-model="form[x.dataIndex!]" :placeholder="`请输入${x.title}`" />
-            </AFormItem>
-          </ACol>
-        </ARow>
-      </ACol>
-      <ADivider direction="vertical" :style="{ height: '72px' }" />
-      <ACol flex="100px">
-        <ASpace direction="vertical" fill>
-          <AButton type="primary" long :loading="$_loading">查询</AButton>
-          <AButton long @click="onReload">重置</AButton>
+  <AForm :model="form" label-align="left" :label-col-props="{ xs: 24, md: 8 }" :wrapper-col-props="{ xs: 24, md: 16 }">
+    <AGrid :collapsed="collapsed" :cols="{ xs: 1, sm: 1, md: 2, lg: 2, xl: 3, xxl: 4 }" :col-gap="16">
+      <AGridItem v-for="x in $columns" :key="x.dataIndex">
+        <AFormItem :label="x.title">
+          <template v-if="x.tips" #label>
+            <span>{{ x.title }}</span>
+            <APopover>
+              <template #content>{{ x.tips }}</template>
+              <IconQuestionCircle />
+            </APopover>
+          </template>
+          <ARangePicker v-if="x.valueType === 'dateRange'" v-model="form[x.dataIndex!]" :style="{ width: '100%' }" />
+          <AInput v-else v-model="form[x.dataIndex!]" :placeholder="`请输入${x.title}`" />
+        </AFormItem>
+      </AGridItem>
+      <AGridItem suffix>
+        <ASpace fill>
+          <AButton type="primary" :loading="$loading">查询</AButton>
+          <AButton @click="onReload">重置</AButton>
+          <AButton type="text" @click="triggerCollapsed">
+            <span>{{ collapsedText }}</span>
+            <IconDown v-if="collapsed" />
+            <IconUp v-else />
+          </AButton>
         </ASpace>
-      </ACol>
-    </ARow>
+      </AGridItem>
+    </AGrid>
   </AForm>
 </template>
 
 <style lang="less" scoped>
+@import: url("@arco-design/web-vue/es/grid/style/index.less");
+
 .pro-query-main {
   margin-bottom: -20px;
 }
--- src/lib/pro-table/keys.ts
+++
@@ -1,8 +0,0 @@
-import type { InjectionKey, ComputedRef } from "vue";
-import type { ProTableColumnData } from "./models";
-
-export const TableColumns = Symbol("TableColumns") as InjectionKey<ComputedRef<ProTableColumnData[]>>;
-
-export const TableLoading = Symbol("TableLoading") as InjectionKey<ComputedRef<boolean>>;
-
-export const TableOnReload = Symbol("TableOnReload") as InjectionKey<() => void>;
