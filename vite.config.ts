54ae0f28f5b0f03e9d1593dec6c26885bb99ed27
Qrans <qran.yue@outlook.com>
2023-10-31 16:52:25 +0800

    依赖更新

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { fileURLToPath, URL } from "node:url";
 
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
-import Component from "unplugin-vue-components/vite";
 import { ArcoResolver } from "unplugin-vue-components/resolvers";
+import Component from "unplugin-vue-components/vite";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
@@ -17,11 +17,6 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       less: { javascriptEnabled: true },
-    },
-  },
-  build: {
-    rollupOptions: {
-      // output: { format: "esm", name: "MicroApp", dir: "dist", file: "main" },
     },
   },
 });
